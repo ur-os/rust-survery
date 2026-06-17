3fd8e852d7b936c9db536ac1ddfbf955a0bca9b4
ur-os <bluiote@gmail.com>
2026-06-17 17:42:45 +0300

    finish calculator

--- rust-survery/src/main.rs
+++ rust-survery/src/main.rs
@@ -1,8 +1,19 @@
 use std::io;
-use std::ops::Add;
-
 fn main() {
     println!("Hello, world!");
+
+    let first_number = &mut 0;
+    let second_number = &mut 0;
+    let operation = &mut ArithmeticOperations::Add;
+
+    let result = read_numbers(first_number, second_number, operation);
+    if result.is_err() {
+        println!("{}", result.err().unwrap().as_str());
+        return
+    }
+
+
+    println!("Result: {}", calculate(*first_number, *second_number, operation))
 }
 
 enum ArithmeticOperations {
@@ -11,7 +22,7 @@ enum ArithmeticOperations {
     Multiply,
     Divide,
 }
-fn calculate(a: i32, b :i32, operation :ArithmeticOperations) -> i64 {
+fn calculate(a: i32, b :i32, operation : &mut ArithmeticOperations) -> i64 {
     match operation {
         ArithmeticOperations::Add => { a as i64 + b as i64 },
         ArithmeticOperations::Subtract => { a as i64 - b as i64 },
@@ -20,19 +31,23 @@ fn calculate(a: i32, b :i32, operation :ArithmeticOperations) -> i64 {
     }
 }
 
-fn read_numbers(left_number: &mut String,
-                right_number: &mut String,
+fn read_numbers(left_number: &mut i32,
+                right_number: &mut i32,
                 operation: &mut ArithmeticOperations) -> Result<(), String> {
     println!("Enter a first number you want to operate:");
+    let left_number_raw= &mut String::new();
     io::stdin()
-        .read_line(left_number)
+        .read_line(left_number_raw)
         .expect("Failed to read first number");
+    *left_number = left_number_raw.trim().parse().expect("Passed incorrect number, supports only i32");
+
 
     println!("Enter a second number you want to operate:");
+    let right_number_raw = &mut String::new();
     io::stdin()
-        .read_line(right_number)
+        .read_line(right_number_raw)
         .expect("Failed to read second number");
-
+    *right_number = right_number_raw.trim().parse().expect("Passed incorrect number, supports only i32");
 
 
     let mut raw_operation= String::new();
@@ -41,7 +56,7 @@ fn read_numbers(left_number: &mut String,
         .read_line(&mut raw_operation)
         .expect("Failed to read arithmetic operation");
 
-    match raw_operation.as_str() {
+    match raw_operation.trim() {
         "+" => { *operation = ArithmeticOperations::Add; },
         "-" => { *operation = ArithmeticOperations::Subtract; },
         "*" => { *operation = ArithmeticOperations::Multiply; },
@@ -51,6 +66,6 @@ fn read_numbers(left_number: &mut String,
 
     }
 
-    return Ok(())
+    Ok(())
 }
 
